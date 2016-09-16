01573fa2d23c043f2b0739d47e32f612fce5a96c
leocardena <leomcardena@hotmail.com>
2016-09-15 21:27:12 -0300

    remocao key

--- src/main/resources/static/app/layouts/background/background.directive.js
+++ src/main/resources/static/app/layouts/background/background.directive.js
@@ -16,7 +16,6 @@
 	     return directive;
 	     
 	     function link (scope, element, attrs, ngModel) {
-	    	 var defaultUrl = 'https://walter.trakt.us/images/shows/000/001/409/fanarts/original/cff0b01ee7.jpg';
 	         return attrs.$observe('backImg', function(value) {
 	             element.css({
 	                 'background-image': 'url(' + value +')',
