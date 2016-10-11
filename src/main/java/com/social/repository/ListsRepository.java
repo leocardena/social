546a6ed315d2b12c1d8dbcd3b7578431704ff522
pepeu07 <Pedro Afonso@PEDRO>
2016-10-11 00:02:04 -0300

    Ajustando a o querydsl

---
+++ src/main/java/com/social/repository/ListsRepository.java
@@ -0,0 +1,10 @@
+package com.social.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.social.domain.Lists;
+
+public interface ListsRepository 
+	extends JpaRepository<Lists, Long>{//extende PagingAndSorting!!!!!!!!!
+
+}
--- src/main/java/com/social/rest/business/ListsBusiness.java
+++ src/main/java/com/social/rest/business/ListsBusiness.java
@@ -7,10 +7,7 @@ import javax.persistence.EntityManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.mysema.query.SearchResults;
-import com.mysema.query.jpa.impl.JPAQuery;
 import com.social.domain.Lists;
-import com.social.domain.QLists;
 
 @Service
 public class ListsBusiness {
@@ -20,22 +17,9 @@ public class ListsBusiness {
 	
 	public List<Lists> getAllListByProfile(){
 
-		QLists qLists = QLists.lists;
-		JPAQuery query = new JPAQuery(em);
-		SearchResults<Lists> listaSearch = query.from(QLists.lists)
-				.where(QLists.lists.profile.id.eq((long) 1))
-				.listResults(qLists);
+		//QLists qLists = QLists.lists;
 		
-		List<Lists> lista = listaSearch.getResults();
-		
-		System.out.println("Total de registros que veio do banco usando Search Result>>>>"+listaSearch.getTotal());
-		System.out.println("Total de registros que veio do banco usando List>>>>"+lista.size());
-		System.out.println("Nome da primeira Lista >>>>"+lista.get(0).getName());
-		System.out.println("Nome da segunda Lista >>>>"+lista.get(1).getName());
-		System.out.println("Nome da primeira Lista >>>>"+lista.get(0).getProfile().getBirthday());
-		System.out.println("Nome da segunda Lista >>>>"+lista.get(1).getProfile().getName());
-		
-		return lista;
+		return null;
 		
 	}
 	
