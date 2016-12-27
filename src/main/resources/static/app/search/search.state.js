0e1f15c029969a96ed8e38d9082dc7b1a92fc299
Leonardo Cardena <leomcardena@hotmail.com>
2016-12-27 00:51:09 -0200

    Correções do merge

--- src/main/resources/static/app/search/search.state.js
+++ src/main/resources/static/app/search/search.state.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 (function() {
     'use strict';
 
@@ -46,58 +45,6 @@
         	
         }
         
-
     }
+    
 })();
-=======
-(function() {
-    'use strict';
-
-    angular
-        .module('social.search')
-        .config(stateConfig);
-
-    stateConfig.$inject = ['$stateProvider'];
-
-    function stateConfig($stateProvider) {
-        $stateProvider
-            .state('search', {
-                parent: 'social',
-                url: '/search/{type}?page?query',
-                data: {
-                    authorities: [],
-                    pageTitle: 'Resultados da Pesquisa'
-                },
-                views: {
-                    'content@': {
-                        templateUrl: 'app/search/search.html',
-                        controller: 'SearchController',
-                        controllerAs: 'vm'
-                    }
-                },
-                resolve: {
-                	searchPrepService : searchPrepService
-                }
-            });
-        
-        searchPrepService.$inject = ['TraktSearchService', '$stateParams'];
-        
-        /*@ngInject*/
-        function searchPrepService (TraktSearchService, $stateParams) {
-        	return TraktSearchService.getSearch({
-        		limit : 18,
-        		page : $stateParams.page,
-        		query : $stateParams.query,
-        		type : $stateParams.type === 'all' ? 'movie,show,person' : $stateParams.type,
-        		fields : 'translations,title'
-        	}).$promise.then(function(data){
-        		return data;
-        	}).catch(function () {
-        	});
-        	
-        }
-        
-
-    }
-})();
->>>>>>> origin/gustavo.fernandes
