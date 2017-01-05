e9a876b5577fabb3325f20448a9bbc758ce7d006
Pedro <Pedro@Pedro-PC.Home>
2017-01-04 23:31:56 -0200

    improvements in query of db.

--- src/main/java/com/social/business/MovieBusiness.java
+++ src/main/java/com/social/business/MovieBusiness.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.querydsl.jpa.impl.JPAQuery;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.social.domain.Movie;
 import com.social.domain.QMovie;
 import com.social.repository.MovieRepository;
@@ -28,27 +29,28 @@ public class MovieBusiness {
 	}
 	
 	public Movie getMovieById(Long id){
-		JPAQuery<?> query = new JPAQuery<Void>(entity);
+		JPAQueryFactory query = new JPAQueryFactory(entity);
 		
 		Movie result = query.select(QMovie.movie)
-				.where(QMovie.movie.id.eq(id)).
-				fetchOne();
+				.where(QMovie.movie.id.eq(id))
+				.fetchOne();
 		
 		return result;
 	}
 
 	public Movie getMovieByName(String name){
-		JPAQuery<?> query = new JPAQuery<Void>(entity);
+		//VERIFICAR JPAQueryFactory para JPAQuery (diference)
+		JPAQueryFactory query = new JPAQueryFactory(entity);
 		
-		Movie result = query.select(QMovie.movie)
+		Movie result = query.selectFrom(QMovie.movie)
 				.where(QMovie.movie.name.eq(name))
-				.fetchOne();
+				.fetchFirst();
 		
 		return result;
 	}
 	
 	public List<Movie> getAllMoviesByName(String name){
-		JPAQuery<?> query = new JPAQuery<Void>(entity);
+		JPAQueryFactory query = new JPAQueryFactory(entity);
 		
 		List<Movie> results = query.select(QMovie.movie)
 				.where(QMovie.movie.name.eq(name)).
