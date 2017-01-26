f709da8d79e554fadc8a46ca542eaf48fa175fa8
Leonardo Cardena <leomcardena@hotmail.com>
2017-01-26 21:35:39 -0200

    URL's ShowREST

--- src/main/java/com/social/web/rest/util/APIEndpoint.java
+++ src/main/java/com/social/web/rest/util/APIEndpoint.java
@@ -10,6 +10,7 @@ public interface APIEndpoint {
 	
 	public final String BASE = "/api";
 	public final String MOVIE = "/api/rest/movies";
+	public final String SHOW = "/api/rest/shows";
 	public final String ACCOUNT = "/api/rest/accounts";
 	public final String AVATAR = "/api/rest/avatars";
 	public final String LISTS = "/api/rest/lists";
